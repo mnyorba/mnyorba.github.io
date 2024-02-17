9ae8f0b786ade25fe40769e6af4dd7dc6b54e1a0
Mikhail Njorba <mnyorba@gmail.com>
2024-02-17 19:43:20 +0200

    [TASK] create start theme with tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://NorbaMykhailo.github.io',
+	integrations: [tailwind({
+		// Example: Disable injecting a basic `base.css` import on every page.
+		// Useful if you need to define and/or import your own custom `base.css`.
+		applyBaseStyles: true,
+		// Example: Allow writing nested CSS declarations
+		// alongside Tailwind's syntax
+		nesting: true,
+	}),
+	],
+});
