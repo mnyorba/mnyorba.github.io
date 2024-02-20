2d72614c18b7e68bc686f0ae30a0f6fe477a8b14
Mikhail Njorba <mnyorba@gmail.com>
2024-02-20 18:20:02 +0200

    [TASK] Generate pages from a collection /content/works/

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
 // Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 
 // Define a `type` and `schema` for each collection
 export const collections = {
 	works: defineCollection({
 		type: 'content',
 		schema: z.object({
+			draft: z.boolean().default(false),
 			title: z.string(),
 			publishDate: z.coerce.date(),
 			description: z.string(),
