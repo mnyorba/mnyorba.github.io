c09d1a2edfe63fdd6fe18f32b62ec656ff84334c
Mikhail Njorba <mnyorba@gmail.com>
2024-02-26 14:07:12 +0200

    [TASK] Add Skills and fix semantic mark up

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,16 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mnyorba.github.io',
-	integrations: [tailwind({
-		// Example: Disable injecting a basic `base.css` import on every page.
-		// Useful if you need to define and/or import your own custom `base.css`.
-		applyBaseStyles: false,
-		// Example: Allow writing nested CSS declarations
-		// alongside Tailwind's syntax
-		nesting: true,
-	}),
+	integrations: [
+		tailwind(
+			{
+				// Example: Disable injecting a basic `base.css` import on every page.
+				// Useful if you need to define and/or import your own custom `base.css`.
+				applyBaseStyles: false,
+				// Example: Allow writing nested CSS declarations
+				// alongside Tailwind's syntax
+				nesting: true,
+			}
+		),
 	],
 });
--- src/pages/about1.md
+++ src/pages/about1.md
@@ -1,8 +1,12 @@
 ---
-title: 'Markdown + Tailwind'
 layout: ../layouts/BaseLayout.astro
+
+title: 'About'
+description: 'About M.Nyorba'
 ---
 
+# About
+<h1 class="text-center">About 1</h1>
 <div class="grid place-items-center h-screen content-center">
  <div class="py-2 px-4 bg-purple-500 text-white font-semibold rounded-lg shadow-md">
   Tailwind classes also work in Markdown!
